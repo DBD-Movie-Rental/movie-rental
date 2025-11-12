080426e13a53a8ca1e6da6121102c3167de9f93d
marcus_rk <makj0005@stud.kea.dk>
2025-11-12 16:31:31 +0100

    Implement MongoDB API routes for customers, employees, inventory, lookups, movies, and rentals

--- app.py
+++ app.py
@@ -2,3 +2,7 @@
 # TODO: add mysql blueprint to flask app
 #from api_routes.mysql import bp as mysql_bp
 #app.register_blueprint(mysql_bp)
+
+# TODO: add mongodb blueprint to flask app
+#from api_routes.mongodb import bp as mongodb_bp
+#app.register_blueprint(mongodb_bp)
